db4c5bbbac007594fb7bcda99ba5f7c7458ce794
Gaston Vazquez <gaston.vazquez@acrons.net>
2021-09-12 18:26:01 -0300

    fix centrado login

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
