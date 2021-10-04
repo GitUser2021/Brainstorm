a4501e0f8b6730264a6b6a1d42d211deae4a1a2e
ags-boop <agustesta2000@gmail.com>
2021-10-04 18:06:39 -0300

    TokenService

--- src/Services/grupo.service.ts
+++ src/Services/grupo.service.ts
@@ -2,26 +2,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Igrupo } from '../app/Models/grupo';
+import { TokenService } from './token.service';
 
-const Token_key = '@@auth0spajs@@::NZlshn18g6678aJBSzkior1rI8sOzkGh::default::openid profile email offline_access';
 
 @Injectable({
   providedIn: 'root'
 })
   
 export class GrupoServiceService {
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient,private TokenService : TokenService) { }
   
-setToken(token: string) {
-    window.localStorage.removeItem(Token_key);
-    window.localStorage.setItem(Token_key, token);
-  }
 
- getToken() {
-    const token = JSON.parse(window.localStorage.getItem(Token_key));
-    return token;
-}
-tokenStruct = this.getToken();
+tokenStruct = this.TokenService.getToken();
   JWT = this.tokenStruct.body.id_token;
 
 HEADERS = {
